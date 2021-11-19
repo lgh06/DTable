98472a6fbc0eb6f90e2921e848c6b961a7f42828
Daniel Liu <hnnk@qq.com>
2021-11-19 09:27:24 +0800

    添加windows启动mongodb网络服务的脚本

---
+++ other_tools/run_mongodb_windows_net_service.bat
@@ -0,0 +1,4 @@
+:: Mongodb官网上的windows版本安装包，默认为网络服务
+:: 若没有运行，执行此脚本可以运行mongdodb的网络服务
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+net start mongodb
